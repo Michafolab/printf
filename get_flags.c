49c562a865a74bfcf9cdaa5cb58c5df90f689c2b
michaelmichvel <michaelngarambe96@gmail.com>
2023-04-25 09:24:26 -0700

    Calculates active flags, Formatted string in which to print the argument

--- get_flags.c
+++ get_flags.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * get_flags -This Calculates active flags
+ * @format:The  Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
