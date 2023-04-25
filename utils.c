637b1a7c45b56d9c7115845de19d3e9ab19d2563
michaelmichvel <michaelngarambe96@gmail.com>
2023-04-25 08:35:21 -0700

    Function that evaluates Char if it's printable

--- utils.c
+++ utils.c
@@ -11,12 +11,13 @@
 /**
 
 
-*printable - Evaluates if a char is printable
+* is_printable - Evaluates if a char is printable
 
 
-*let@c: Char to be evaluated.
+* @c: Char to be evaluated.
 
 
+*
 
 
 * Return: 1 if c is printable, 0 otherwise
